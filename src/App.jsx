607937eed66946a690170645eb2f4e3c538576a0
Leo Fudaly <leofudaly@MacBook-Air-uzivatela-Leo.local>
2026-02-18 13:29:42 +0100

    scroloveniemimo

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState ,useEffect} from 'react'
 
 import './App.css'
 
@@ -10,16 +10,42 @@ import Header from './komponenty/header/header'
 import Uvod from './pages/uvod/uvod'
 import Uvod2 from './pages/uvod2/uvod2'
 function App() {
+  
   const [count, setCount] = useState(0)
+  const[nastavenia,setNastaveni] = useState(0)
 
+
+  // console.log(vyska);
+// useEffect(() => {
+//   const handleResize = () => {
+//     const vyska = window.innerHeight;
+//     setNastaveni(vyska);
+//     console.log("Resize viewportu:", vyska);
+//   };
+
+//   window.addEventListener("resize", handleResize);
+//   return () => window.removeEventListener("resize", handleResize);
+// }, [])
+useEffect(() => {
+  const funkcia = () => {
+    const vyska = window.innerHeight;
+    setNastaveni(vyska);
+    console.log("Aktuálna výška viewportu:", nastavenia); // použijeme vyska, nie state
+  }
+
+  window.addEventListener("resize", funkcia);
+
+  return () => window.removeEventListener("resize", funkcia);
+}, []);
+  
   return (
     <>
     <Header></Header>
 
     <Uvod></Uvod>
+    <Uvod2></Uvod2>
     <Uvod></Uvod>
-    <Uvod></Uvod>
-    <Uvod></Uvod>
+    <Uvod2></Uvod2>
   
     </>
   )
--- src/pages/uvod/uvod.jsx
+++ src/pages/uvod/uvod.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 
 const Uvod = () => {
   return (
-    <div className='min-h-[800px] w-full bg-slate-500'>KOKOKOKOKOKOKOKO
+    <div className='h-[800px] w-full bg-slate-500 flex p-2'>
+      <div className='w-full h-full bg-slate-50'></div>
       
     </div>
   )
