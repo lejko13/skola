8f035034aefcfa912286b364a70a1b4f76ab0f97
Leo Fudaly <leofudaly@MacBook-Air-uzivatela-Leo.local>
2026-02-18 10:07:51 +0100

    vyriesenieobashu

--- src/pages/uvod/uvod.jsx
+++ src/pages/uvod/uvod.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Uvod = () => {
   return (
-    <div className='min-h-[100svh] w-full bg-slate-500'>KOKOKOKOKOKOKOKO
+    <div className='min-h-[100dvh] w-full bg-slate-500'>KOKOKOKOKOKOKOKO
       
     </div>
   )
