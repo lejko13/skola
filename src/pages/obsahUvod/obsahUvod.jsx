45c768fa1a3ccc0afd136d0dd59c83226ddbf789
Leo Fudaly <leofudaly@MacBook-Air-uzivatela-Leo.local>
2026-02-22 20:05:36 +0100

    jojoj

--- src/pages/obsahUvod/obsahUvod.jsx
+++ src/pages/obsahUvod/obsahUvod.jsx
@@ -37,77 +37,9 @@ setvyska(pole[0].contentRect.height +144)
   <div
   ref={referencia}
    className="w-full h-fit flex flex-col  gap-3 justify-around  md:h-full bg-slate-500">
+<div className="h-[200px]"></div>
 
-    {/* {isMdUp && 
-    
-      } */}
-
-
-    
-    {/* vrch */}
-        <div className='w-full h-fit flex  bg-red-400'>
-
-          <div className='flex flex-col  leading-none w-fit'>
-              <span className='text-[30vw] font-[550] text-whiteCustom md:text-[180px]'>
-                {nazovhalvny}
-              </span>
-              <span className='text-[13vw] font-[550]  text-whiteCustom'>
-                {studio}
-              </span>
-            </div>
-
-          <div className='w-fit  pt-[2%]' >
-              <div className=' h-fit w-full aspect-[9/9]  flex items-center justify-start  text-whiteCustom '>
-                        <i class='bx bx-registered  text-[9vw]  prvy:text-[10vw] druhy:text-[11vw] sm:text-[12vw]' ></i>
-                      </div>
-          </div>
-
-        </div>
-    {/* vrch */}
-
-
-    {/* textinfo */}
-    <div className='w-full h-fit flex flex-col items-end md:items-start'>
-    {data.map((item) => 
-    <div className='text-[14px]  druhy:text-[16px] font-[500] text-whiteCustom cursor-pointer ' >{item}</div>
-    )}
-    </div>
-    {/* textinfo */}
-
-
-    {/* karta */}
-    <div className='w-full  h-fit'>
-    <Karta></Karta>
-      
-    </div>
-    {/* karta */}
-
-
-    {/* tocenveeta */}
-    <div className='w-full  h-fit'>
-    <Tocenaveta
-    veta1 =   {veta1}
-    veta2 = {veta2}
-    ></Tocenaveta>
-    </div>
-    {/* tocenveeta */}
-
-    {/* spodok */}
-    <div className='w-full  h-fit'>
-        <Malytext
-          rok = {rok}
-          nazov = {nazovhalvny}
-          studio =     {studio}
-          ></Malytext>
-
-    </div>
-    {/* spodok */}
-
-
-  
-    
-
-
+   
 
 
 
@@ -122,3 +54,70 @@ setvyska(pole[0].contentRect.height +144)
 }
 
 export default ObsahUvod
+
+
+//  {isBelowMd && 
+//     <>
+//     {/* vrch */}
+//         <div className='w-full h-fit flex  bg-red-400'>
+
+//           <div className='flex flex-col  leading-none w-fit'>
+//               <span className='text-[30vw] font-[550] text-whiteCustom md:text-[180px]'>
+//                 {nazovhalvny}
+//               </span>
+//               <span className='text-[13vw] font-[550]  text-whiteCustom'>
+//                 {studio}
+//               </span>
+//             </div>
+
+//           <div className='w-fit  pt-[2%]' >
+//               <div className=' h-fit w-full aspect-[9/9]  flex items-center justify-start  text-whiteCustom '>
+//                         <i class='bx bx-registered  text-[9vw]  prvy:text-[10vw] druhy:text-[11vw] sm:text-[12vw]' ></i>
+//                       </div>
+//           </div>
+
+//         </div>
+//     {/* vrch */}
+
+
+//     {/* textinfo */}
+//     <div className='w-full h-fit flex flex-col items-end md:items-start'>
+//     {data.map((item) => 
+//     <div className='text-[14px]  druhy:text-[16px] font-[500] text-whiteCustom cursor-pointer ' >{item}</div>
+//     )}
+//     </div>
+//     {/* textinfo */}
+
+
+//     {/* karta */}
+//     <div className='w-full  h-fit'>
+//     <Karta></Karta>
+      
+//     </div>
+//     {/* karta */}
+
+
+//     {/* tocenveeta */}
+//     <div className='w-full  h-fit'>
+//     <Tocenaveta
+//     veta1 =   {veta1}
+//     veta2 = {veta2}
+//     ></Tocenaveta>
+//     </div>
+//     {/* tocenveeta */}
+
+//     {/* spodok */}
+//     <div className='w-full  h-fit'>
+//         <Malytext
+//           rok = {rok}
+//           nazov = {nazovhalvny}
+//           studio =     {studio}
+//           ></Malytext>
+
+//     </div>
+//     {/* spodok */}
+
+
+//     </>
+//     }
+
