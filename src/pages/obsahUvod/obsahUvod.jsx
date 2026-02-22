56e54eefc022c9f199d79ce50e18b8a5cae390b8
Leo Fudaly <leofudaly@MacBook-Air-uzivatela-Leo.local>
2026-02-22 19:55:12 +0100

    kar

--- src/pages/obsahUvod/obsahUvod.jsx
+++ src/pages/obsahUvod/obsahUvod.jsx
@@ -36,7 +36,7 @@ setvyska(pole[0].contentRect.height +144)
 
   <div
   ref={referencia}
-   className="w-full h-[800px] min-h-[800px] flex flex-col  gap-3 justify-around  md:h-full">
+   className="w-full h-fit flex flex-col  gap-3 justify-around  md:h-full bg-slate-500">
 
     {/* {isMdUp && 
     
