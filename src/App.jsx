3eaeeb1144290af1be56cc00bf50693db4a75399
Leo Fudaly <leofudaly@MacBook-Air-uzivatela-Leo.local>
2026-03-06 15:57:45 +0100

    kar

--- src/App.jsx
+++ src/App.jsx
@@ -181,6 +181,28 @@ useEffect(() => {
   
 
 const[hoverkoko,setHoverko] = useState(null)
+
+
+
+
+
+
+
+ useEffect(() => {
+            if (skusam) {
+              document.body.style.overflow = "hidden";
+              document.documentElement.style.overflow = "hidden"; // zablokuje scroll aj na html
+            } else {
+              document.body.style.overflow = "auto";
+              document.documentElement.style.overflow = "auto"; // obnoví scroll
+            }
+          
+            // cleanup pri unmount
+            return () => {
+              document.body.style.overflow = "auto";
+              document.documentElement.style.overflow = "auto";
+            };
+          }, [skusam]);
   return (
     <>
 
